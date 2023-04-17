2319b7f840d0c2eca96ee8e0364ea43a34201e8f
dumbus <dumbusx@gmail.com>
2023-04-18 00:02:07 +0300

    feat: implement counting of differences

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import { dataValidator } from './helpers/validation.js';
+import { countDifferences } from './helpers/countDifferences.js';
 
-const testData = [
-  '7109cc53 ff266014 847ee03a 5f46bd42', // is ok
-  '7109cc53 ff266014 847ee03a 5f46bd4а', // has inexpected symbols
-  '000000' // length is wrong
+// const testErrorsData = [
+//   '7109cc53 ff266014 847ee03a 5f46bd42', // is ok
+//   '7109cc53 ff266014 847ee03a 5f46bd4а', // has unexpected symbols
+//   '000000' // length is wrong
+// ];
+
+const testwithoutOneData = [
+  '7109cc53 ff266014 847ee03a 5f46bd42',
+  'b831192c 60ddbdfa b0e7e6ad d84b3370'
 ];
 
-dataValidator(testData, 'с \"1\"');
+const testwithOneData = [
+  '7109cd53 ff266014 847ee03a 5f46bd42',
+  'b831192c 60ddbdfa 46e6115b d84b3370'
+];
+
+// program should show: 
+// 1
+// 7
+
+dataValidator(testwithoutOneData, 'без \"1\"');
+dataValidator(testwithOneData, 'с \"1\"');
+
+countDifferences(testwithoutOneData, testwithOneData);
