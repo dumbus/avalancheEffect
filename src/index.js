3a83b0eedd711f6252e899f7fd1376a577a09dbb
dumbus <dumbusx@gmail.com>
2023-04-18 23:53:57 +0300

    docs: add README docs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import * as dotenv from 'dotenv';
 
-import { getAppMode } from './app/parsers/parseAppMode.js';
-import { getData } from './app/parsers/parseDataFromUser.js';
+import { withoutOneDataStr, withOneDataStr } from './data.js';
 import { solver } from './app/solver.js';
 
 dotenv.config();
 
-const appMode = getAppMode();
-
-const data = getData(appMode);
+const data = { withoutOneDataStr, withOneDataStr };
 
 solver(data);
